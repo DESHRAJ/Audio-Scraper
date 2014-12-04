570e476cf2e9df844bff5a7c0f7d49ab0a2b81ed
deshraj <deshrajdry@gmail.com>
2014-12-04 07:27:23 +0530

    django configured fully

--- djangoaudio/djangoaudio/urls.py
+++ djangoaudio/djangoaudio/urls.py
@@ -1,17 +1,24 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
+from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse
+import settings
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'djangoaudio.views.home', name='home'),
+    url(r'^$', 'djangoaudio.views.home', name='home'),
     # url(r'^djangoaudio/', include('djangoaudio.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
---
+++ djangoaudio/djangoaudio/views.py
@@ -0,0 +1,17 @@
+from django.core.context_processors import csrf
+from django.template import RequestContext
+from django.shortcuts import render_to_response, render
+from django.http import Http404, HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.http import *
+from django.conf import settings
+# import mongoengine
+from django.core.mail import send_mail
+from django.template.loader import render_to_string, get_template
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
+import random,string
+
+
+def home(request):
+	return HttpResponse("<h1>Under Construction... Will be there very soon </h1>")
