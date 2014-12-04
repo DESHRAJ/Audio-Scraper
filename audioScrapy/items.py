34c4bbe1218aa895ee2398c0768d2ee2c5b61830
deshraj <deshrajdry@gmail.com>
2014-12-04 08:13:44 +0530

    scrapy setup complete and its integration with django is done

--- audioScrapy/items.py
+++ audioScrapy/items.py
@@ -6,7 +6,9 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from scrapy.item import Item, Field
+from scrapy.contrib.djangoitem import DjangoItem
+from djangoaudio.models import * 
 
 class AudioscrapyItem(scrapy.Item):
     # define the fields for your item here like:
--- audioScrapy/pipelines.py
+++ audioScrapy/pipelines.py
@@ -5,7 +5,14 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+# class AudioscrapyPipeline(object):
+#     def process_item(self, item, spider):
+#         return item
+
+from scrapy.exceptions import DropItem
+from djangoaudio.models import *
 
 class AudioscrapyPipeline(object):
-    def process_item(self, item, spider):
-        return item
+
+	def process_item(self,item,spider):
+		return item
