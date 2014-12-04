4973d0f9320ba76495746f7cb9d3d93196e5d0cd
deshraj <deshrajdry@gmail.com>
2014-12-04 06:57:40 +0530

    scrapy and django setup

---
+++ audioScrapy/audioScrapy/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for audioScrapy project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'audioScrapy'
+
+SPIDER_MODULES = ['audioScrapy.spiders']
+NEWSPIDER_MODULE = 'audioScrapy.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'audioScrapy (+http://www.yourdomain.com)'
